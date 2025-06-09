43e4af809a684fa9df0c5decbe8eecf21398505a
girvel <widauka@ya.ru>
2025-06-09 23:39:01 +0500

    Reflecting system's types

--- src/main.zig
+++ src/main.zig
@@ -8,36 +8,52 @@ fn display_y(entity: ContainsY) void {
     std.debug.print("y = {}\n", .{entity.y.*});
 }
 
-const World = struct {
-    ys: std.ArrayList(i32),
-    display_y_subjects: std.ArrayList(ContainsY),
+fn BuildWorld(only_system: anytype) type {
+    const system_info = @typeInfo(@TypeOf(only_system));
+    const Argument: type = system_info.Fn.params[0].type orelse unreachable;
+    const argument_only_field: std.builtin.Type.StructField
+        = @typeInfo(Argument).Struct.fields[0];
+    const field_name = argument_only_field.name;
+    const FieldType = @typeInfo(argument_only_field.type).Pointer.child;
 
-    fn add(self: *World, entity: anytype) void {
-        const t = @TypeOf(entity);
-        if (@hasField(t, "y")) {
-            if (@TypeOf(@field(entity, "y")) != i32) {
-                @compileError("entity's .y should be of type i32");
+    return struct {
+        // TODO bad field naming
+        // TODO world contains components, systems contain entities
+        ys: std.ArrayList(FieldType),
+        display_y_subjects: std.ArrayList(Argument),
+
+        const Self = @This();
+        fn add(self: *Self, entity: anytype) void {
+            const t = @TypeOf(entity);
+            if (@hasField(t, field_name)) {
+                if (@TypeOf(@field(entity, field_name)) != FieldType) {
+                    @compileError(
+                        "entity's ." ++ field_name ++
+                        " should be of type " ++ @typeName(FieldType)
+                    );
+                }
+
+                self.ys.append(@field(entity, field_name)) catch unreachable;
+                var subject: Argument = undefined;
+                @field(subject, field_name) = &self.ys.items[self.ys.items.len - 1];
+                self.display_y_subjects.append(subject) catch unreachable;
             }
-
-            self.ys.append(@field(entity, "y")) catch unreachable;
-            self.display_y_subjects.append(ContainsY {
-                .y = &self.ys.items[self.ys.items.len - 1],
-            }) catch unreachable;
         }
-    }
 
-    fn update(self: *World) void {
-        for (self.display_y_subjects.items) |e| {
-            display_y(e);
+        fn update(self: *Self) void {
+            for (self.display_y_subjects.items) |e| {
+                @call(.auto, only_system, .{e});
+            }
         }
-    }
-};
+    };
+}
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    var world = World {
+    // TODO .init
+    var world = BuildWorld(display_y) {
         .ys = std.ArrayList(i32).init(allocator),
         .display_y_subjects = std.ArrayList(ContainsY).init(allocator),
     };
