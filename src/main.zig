6101a2d0ae23f25c51c2099b1406fcc3528bb3e0
girvel <widauka@ya.ru>
2025-06-14 16:23:47 +0500

    Benchmark thread vs nothread on vector addition

--- src/main.zig
+++ src/main.zig
@@ -17,42 +17,55 @@ const Constants = struct {
 };
 
 fn only_system(entity: Inert, constants: Constants) void {
-    std.debug.print("g = {}\n", .{constants.g.*});
     entity.velocity.add_mut(constants.g.*);
     entity.position.add_mut(entity.velocity.*);
 }
 
 
-const ENTITIES_N = 100;
+const ENTITIES_N = 10_000_000;
+const TICKS = 10;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    var world = ecs.BuildWorld(only_system, null).init(allocator);
+    std.debug.print("{} entities, {} ticks\n", .{ENTITIES_N, TICKS});
 
-    // TODO handle dangling pointers bug
-    world.components.position.ensureTotalCapacity(ENTITIES_N) catch unreachable;
-    world.components.velocity.ensureTotalCapacity(ENTITIES_N) catch unreachable;
+    inline for ([_]?usize {null, 0}) |threading_i| {
+        var world = ecs.BuildWorld(only_system, threading_i).init(allocator);
 
-    for (0..ENTITIES_N) |_| {
-        const  x = std.crypto.random.intRangeAtMost(i32, -1000, 1000);
-        const  y = std.crypto.random.intRangeAtMost(i32, -1000, 1000);
-        const vx = std.crypto.random.intRangeAtMost(i32, -1000, 1000);
-        const vy = std.crypto.random.intRangeAtMost(i32, -1000, 1000);
+        // TODO handle dangling pointers bug
+        world.components.position.ensureTotalCapacity(ENTITIES_N) catch unreachable;
+        world.components.velocity.ensureTotalCapacity(ENTITIES_N) catch unreachable;
+
+        for (0..ENTITIES_N) |_| {
+            const  x = std.crypto.random.intRangeAtMost(i32, -1000, 1000);
+            const  y = std.crypto.random.intRangeAtMost(i32, -1000, 1000);
+            const vx = std.crypto.random.intRangeAtMost(i32, -1000, 1000);
+            const vy = std.crypto.random.intRangeAtMost(i32, -1000, 1000);
+            world.add(.{
+                .position = i32_2.from_array(.{x, y}),
+                .velocity = i32_2.from_array(.{vx, vy}),
+            });
+        }
+
         world.add(.{
-            .position = i32_2.from_array(.{x, y}),
-            .velocity = i32_2.from_array(.{vx, vy}),
+            .g = i32_2.from_array(.{0, 10}),
+        });
+
+        var timer = std.time.Timer.start() catch unreachable;
+        const start = timer.lap();
+
+        for (0..TICKS) |_| {
+            world.update();
+        }
+
+        const end = timer.read();
+        std.debug.print("{s}: {d:.2} FPS\n", .{
+            if (threading_i == null) "nothread" else "thread  ",
+            1e9 * TICKS / @as(f64, @floatFromInt(end - start)),
         });
     }
-
-    world.add(.{
-        .g = i32_2.from_array(.{0, 10}),
-    });
-
-    world.update();
-    world.update();
-    world.update();
 }
 
 test {
--- src/vector.zig
+++ src/vector.zig
@@ -18,7 +18,6 @@ pub fn Vector(comptime element_type: type, comptime length: usize) type {
         }
 
         pub fn add_mut(self: *Self, other: Self) void {
-            std.debug.print("{} + {}\n", .{self, other});
             for (&self.items, other.items) |*a, b| {
                 a.* += b;
             }
