719da433fb50d7daed6d3377cd26b81d6bace574
girvel <widauka@ya.ru>
2025-07-28 12:45:32 +0500

    ECS+raylib

--- plan.md
+++ plan.md
@@ -1,14 +1,8 @@
-## Concept
-
-ECS implementation allowing for multithreaded systems. *Potential expansion:* systems running in parallel with each other.
-
-## Tasks
-
 - [x] Hello world
 - [x] Manual world + system
 - [ ] Mono-world -> multi-world
   - [x] Multiple components required
   - [x] Multiple arguments in a system
+  - [x] One system threading
   - [ ] System as a separate entity
-  - [ ] System-level threading
   - [ ] Multiple systems in a world
--- src/main.zig
+++ src/main.zig
@@ -2,19 +2,47 @@ const std = @import("std");
 const rl = @import("raylib");
 const vector = @import("vector.zig");
 const i32_2 = vector.Vector(i32, 2);
+const ecs = @import("ecs.zig");
+
+// TODO sprite or text
+const Drawable = struct {
+    position: *i32_2,
+    sprite: *rl.Texture2D,
+};
+
+fn draw(target: Drawable) void {
+    const pos = target.position.*.items;
+    rl.drawTexture(target.sprite.*, pos[0], pos[1], .white);
+}
 
 pub fn main() !void {
     const window_size = i32_2.from_array(.{100, 100});
     rl.initWindow(window_size.items[0], window_size.items[1], "Zig ECS test");
     defer rl.closeWindow();
 
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    var world = ecs.BuildWorld(draw, .none).init(allocator);
+
     const mannequin = try rl.loadTexture("assets/mannequin.png");
+    const moose_dude = try rl.loadTexture("assets/moose_dude.png");
+
+    world.add(.{
+        .position = i32_2.from_array(.{0, 0}),
+        .sprite = moose_dude,
+    });
+
+    world.add(.{
+        .position = i32_2.from_array(.{16, 0}),
+        .sprite = mannequin,
+    });
 
     while (!rl.windowShouldClose()) {
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(.white);
-        rl.drawTexture(mannequin, 0, 0, .white);
+        world.update();
     }
 }
